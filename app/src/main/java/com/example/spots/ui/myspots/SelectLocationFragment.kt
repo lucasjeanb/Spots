c1ff2a5dd6ef0c819170101b83ba853afa628a05
Lucas <lucasjeanb@gmail.com>
2019-11-10 22:40:02 -0600

    ui change

---
+++ app/src/main/java/com/example/spots/ui/myspots/SelectLocationFragment.kt
@@ -0,0 +1,26 @@
+package com.example.spots.ui.myspots
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.example.spots.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class SelectLocationFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_select_location, container, false)
+    }
+
+
+}
